c1a58779bf7815ba91fc834f51d9339f80aab571
afrye1997 <allisonfrye97@gmail.com>
2020-11-06 08:48:58 -0600

    fixed

--- connection.js
+++ connection.js
@@ -3,9 +3,9 @@ const mysql= require('mysql')
 
 const connection= mysql.createConnection({
     host:process.env.HOST,
-    user:"admin",
+    user:process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database:"csce4623"
+    database:process.env.DB_INSTANCE
 });
 
 module.exports = connection;
