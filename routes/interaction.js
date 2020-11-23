467e908d6fd50ce7cde0e904acdbcb9b58233667
afrye1997 <allisonfrye97@gmail.com>
2020-11-22 22:43:28 -0600

    idk

---
+++ routes/interaction.js
@@ -0,0 +1,11 @@
+"use  strict";
+const express = require("express");
+const router = express.Router();
+var connection = require("../connection");
+
+
+/**
+ * Interaction actions are defined as: 
+ * 0= 
+ * 1=
+ */
--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,34 @@ const express = require("express");
 const router = express.Router();
 var connection = require("../connection");
 
+
+/**
+ * create table USERS
+(
+	userID varchar(50) not null,
+	userFNAME varchar(50) not null,
+	userLNAME varchar(50) not null,
+	userEMAIL varchar(45) not null,
+	userHOMETOWN varchar(45) null,
+	userGENDER varchar(45) null,
+	userPROFILEPIC varchar(50) null,
+	userGRADE_LEVEL int null,
+	userABOUT varchar(150) null,
+	userGRAD_DATE varchar(100) null,
+	userMAJOR varchar(50) null,
+	userCLASSES varchar(150) null,
+	userPROGRAM_EXP int null,
+	constraint userID_UNIQUE
+		unique (userID)
+);
+
+alter table USERS
+	add primary key (userID);
+
+
+ */
+
+ //GET THE USER
 router.route("/getUser").get(async (req, res, next) => {
   //so param is called USER_id
   const USER_id = req.query.USER_id;
@@ -32,6 +60,56 @@ router.route("/getUser").get(async (req, res, next) => {
   );
 });
 
+/**
+ *  {
+    "userID":"af027",
+    "values":{
+        "userHOMETOWN":"Kansas City",
+        "userMAJOR":"Computer Science",
+        "userGRAD_DATE":"May '21"
+    }
+ }
+
+ */
+
+// UPDATE THE USER
+router.route("/updateUser").post(async (req, res, next) => {
+  console.log("updating user");
+  const request = req.body;
+  var values = request.values;
+  var TABLE = "UPDATE USERS";
+  var VALUES_TO_UPDATE = "SET ";
+  var USER = `WHERE userID= "${request.userID}"`;
+
+  for (column in values) {
+    var value = values[column];
+    VALUES_TO_UPDATE += `${column} = "${value}",`;
+  }
+  VALUES_TO_UPDATE = VALUES_TO_UPDATE.slice(0, -1);
+  const UPDATE_USER_IN_USERTABLE = `${TABLE} 
+                                   ${VALUES_TO_UPDATE} 
+                                   ${USER}`;
+
+
+  connection.query(UPDATE_USER_IN_USERTABLE, function (error, results, fields) {
+  if (error) {
+    console.log("User was NOT successfully updated!")
+    return res.status(400).send({
+      isError: true,
+      result: "Error in updating interests table",
+    });
+  } else {
+    console.log("User was successfully updated!")
+    return res.status(200).send({
+      isError: false,
+      result: "Successfully updated USER",
+    });
+  }
+});
+});
+
+
+// ADD USER
 router.route("/addUser").post(async (req, res, next) => {
   console.log("addUser was called");
   const { USER } = req.body;
